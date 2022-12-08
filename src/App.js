9b6223692c7a442cbf10aed43166402ccea33865
Keshwar04 <97958777+Keshwar04@users.noreply.github.com>
2022-12-08 18:23:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import { arr } from "./DummyData";
+import "./styles.css";
+
+export default function App() {
+  const [search, setSearch] = useState([]);
+
+  return (
+    <div className="App">
+      <div>
+        <input
+          placeholder="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <FiSearch className="searchbar" />
+      </div>
+      <br />
+      <table>
+        <thead>
+          <tr>
+            <th>FName</th>
+            <th>LName</th>
+            <th>Age</th>
+            <th>City</th>
+            <th>Pincode</th>
+          </tr>
+        </thead>
+        {arr
+          .filter((e) => {
+            if (e.fname.toLowerCase().includes(search)) {
+              return e;
+            } else if (e.lname.toLowerCase().includes(search)) {
+              return e;
+            } else if (e.age.toString().includes(search)) {
+              return e;
+            } else if (e.city.toLowerCase().includes(search)) {
+              return e;
+            } else if (e.pincode.toString().includes(search)) {
+              return e;
+            }
+          })
+          .map((e, i) => (
+            <tbody key={i}>
+              <tr>
+                <td>&nbsp; &nbsp;{e.fname}</td>
+                <td>{e.lname}</td>
+                <td>{e.age}</td>
+                <td>{e.city}</td>
+                <td>{e.pincode}</td>
+              </tr>
+            </tbody>
+          ))}
+      </table>
+    </div>
+  );
+}
---
+++ src/DummyData.js
@@ -0,0 +1,37 @@
+export var arr = [
+  {
+    fname: "karthi",
+    lname: "keshwar",
+    age: 28,
+    city: "chennai",
+    pincode: 603406
+  },
+  {
+    fname: "santhosh",
+    lname: "kumar",
+    age: 26,
+    city: "Blr",
+    pincode: 603405
+  },
+  {
+    fname: "yukesh",
+    lname: "waran",
+    age: 22,
+    city: "Trichy",
+    pincode: 603407
+  },
+  {
+    fname: "Saravana",
+    lname: "kumar",
+    age: 25,
+    city: "chennai",
+    pincode: 603405
+  },
+  {
+    fname: "keshwar",
+    lname: "karthi",
+    age: 25,
+    city: "chennai",
+    pincode: 603406
+  }
+];
